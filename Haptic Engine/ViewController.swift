a480ed1035df658bcb6dd70866807acffd36eca8
Raghav Mangrola <raghav.mangrola@eventfarm.com>
2018-01-13 12:00:45 -0800

    Initial Commit

---
+++ Haptic Engine/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Haptic Engine
+//
+//  Created by Raghav Mangrola on 1/13/18.
+//  Copyright © 2018 Raghav Mangrola. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
